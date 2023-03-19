333e6a963e1f18b27dccce0defe1dae48ef813de
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-03-19 17:34:01 +0200

    re-commit

--- src/index.js
+++ src/index.js
@@ -9,30 +9,30 @@ import { fetchCountries } from './js/fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
 
-function fetchCountries (searchQuery) {
-    return fetch('https://restcountries.eu/rest/v2/name/' + searchQuery)
-    .then(response => response.json())
-    .catch(error => console.log(error));
-}
-const refs = {
-    searchForm: document.querySelector('#search-box'),
-    countriesList: document.querySelector('.country-list'),
-    countryCard: document.querySelector('.country-info'),
-};
-console.log(refs);
-refs.searchForm.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+// function fetchCountries (searchQuery) {
+//     return fetch('https://restcountries.eu/rest/v2/name/' + searchQuery)
+//     .then(response => response.json())
+//     .catch(error => console.log(error));
+// }
+// const refs = {
+//     searchForm: document.querySelector('#search-box'),
+//     countriesList: document.querySelector('.country-list'),
+//     countryCard: document.querySelector('.country-info'),
+// };
+// console.log(refs);
+// refs.searchForm.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
-function onSearch(event){
-    event.preventDefault();
-    const searchQuery = event.target.value;
-    if(searchQuery === ''){
-        return;
-    }
-    fetchCountries(searchQuery);
-    console.log(searchQuery);
-}
+// function onSearch(event){
+//     event.preventDefault();
+//     const searchQuery = event.target.value;
+//     if(searchQuery === ''){
+//         return;
+//     }
+//     fetchCountries(searchQuery);
+//     console.log(searchQuery);
+// }
 
-console.log(fetchCountries);
+// console.log(fetchCountries);
 
 
 
